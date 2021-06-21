08ce4727842849db0a1798c8b1a03941e598d28d
Arturbarth <arturbarth@hotmail.com>
2021-06-20 21:01:12 -0300

    Commit Inicial

---
+++ src/main/java/com/arturbarth/VotosAPI/VotosApiApplication.java
@@ -0,0 +1,13 @@
+package com.arturbarth.VotosAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturbarth/VotosAPI/VotosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturbarth.VotosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
